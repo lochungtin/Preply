b1fe88919a04960827e0c7fadccc765c320ef17c
lochungtin <lochungtin@gmail.com>
2021-05-11 01:23:27 +0800

    screen integration

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import { StatusBar, Text, View, } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import { connect } from 'react-redux';
-import { TodoType } from '../types';
+
+import { SettingsType } from '../types';
+import TodoScreen from '../screens/TodoScreen';
+import NoteScreen from '../screens/NoteScreen';
+import RoutineScreen from '../screens/RoutineScreen';
+import CalculatorScreen from '../screens/CalculatorScreen';
+import ConverterScreen from '../screens/ConverterScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
 interface ReduxProps {
-    todo: Array<TodoType>,
+	settings: SettingsType,
 }
+
+const RootNav = createDrawerNavigator();
 
 class AppNav extends React.Component<ReduxProps> {
 	render() {
 		return (
-			<View>
-				<StatusBar
-					backgroundColor="#0e0e0e"
-				/>
-				<Text>
-					{this.props.todo.length}
-				</Text>
-			</View>
+			<NavigationContainer>
+				<StatusBar backgroundColor="#e0e0e0" />
+				<RootNav.Navigator>
+					<RootNav.Screen name="To-Dos" component={TodoScreen} />
+					<RootNav.Screen name="Notes" component={NoteScreen} />
+					<RootNav.Screen name="Routines" component={RoutineScreen} />
+					<RootNav.Screen name="Calculator" component={CalculatorScreen} />
+					<RootNav.Screen name="Unit Converter" component={ConverterScreen} />
+					<RootNav.Screen name="Settings" component={SettingsScreen} />
+				</RootNav.Navigator>
+			</NavigationContainer>
 		);
 	}
 }
 
 const mapStateToProps = (state: ReduxProps) => ({
-    todo: state.todo,
+    settings: state.settings,
 });
 
 export default connect(mapStateToProps)(AppNav);
