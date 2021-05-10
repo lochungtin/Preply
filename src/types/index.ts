f8a336f41ad078f5d64f90380f53f489d804d01c
lochungtin <lochungtin@gmail.com>
2021-05-10 23:32:52 +0800

    type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,45 @@
+import { repeatType } from './routineTypes';
+
+// data types
+export interface DateType {
+    creation: string,
+    modified: string,
+}
+
+export interface TagType {
+    key: string,
+    name: string,
+    color: string,
+}
+
+export interface TodoType {
+    key: string,
+    title: string,
+    date: DateType,
+    tags: Array<TagType>,
+    content: string,
+}
+
+export interface NoteType {
+    key: string,
+    title: string,
+    date: DateType,
+    tags: Array<TagType>,
+    content: string,
+}
+
+export interface RoutineType {
+    key: string,
+    title: string,
+    date: string,
+    time: string,
+    repeatType: repeatType,
+    tags: Array<TagType>,
+    content: string,
+}
+
+// other types
+export interface ActionType {
+    type: string,
+    payload?: any
+}
---
+++ src/types/routineTypes.ts
@@ -0,0 +1,5 @@
+export enum repeatType {
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+}
