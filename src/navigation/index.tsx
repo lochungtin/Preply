39ec4f4fe405e31db3747aa73c1c91940d62d1d2
lochungtin <lochungtin@gmail.com>
2021-05-11 02:36:46 +0800

    custom drawer with labels

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
 import { SettingsType } from '../types';
@@ -11,25 +11,95 @@ import RoutineScreen from '../screens/RoutineScreen';
 import CalculatorScreen from '../screens/CalculatorScreen';
 import ConverterScreen from '../screens/ConverterScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import AccountScreen from '../screens/AccountScreen';
 
 interface ReduxProps {
 	settings: SettingsType,
 }
 
 const RootNav = createDrawerNavigator();
+const customContent = (props: any) => {
+	console.log(props.descriptors);
+	return (
+		<DrawerContentScrollView {...props}>
+			<Text>
+				Preply
+			</Text>
+			<DrawerItem
+				label="To-Dos"
+				onPress={() => props.navigation.navigate('todos')}
+			/>
+			<DrawerItem
+				label="Notes"
+				onPress={() => props.navigation.navigate('notes')}
+			/>
+			<DrawerItem
+				label="Routines"
+				onPress={() => props.navigation.navigate('routines')}
+			/>
+			<Text>Utilities</Text>
+			<DrawerItem
+				label="Calculator"
+				onPress={() => props.navigation.navigate('calculator')}
+			/>
+			<DrawerItem
+				label="Unit Converter"
+				onPress={() => props.navigation.navigate('converter')}
+			/>
+			<Text>Others</Text>
+			<DrawerItem
+				label="Account"
+				onPress={() => props.navigation.navigate('account')}
+			/>
+			<DrawerItem
+				label="Settings"
+				onPress={() => props.navigation.navigate('settings')}
+			/>
+		</DrawerContentScrollView>
+	);
+}
 
 class AppNav extends React.Component<ReduxProps> {
 	render() {
 		return (
 			<NavigationContainer>
-				<StatusBar backgroundColor="#e0e0e0" />
-				<RootNav.Navigator>
-					<RootNav.Screen name="To-Dos" component={TodoScreen} />
-					<RootNav.Screen name="Notes" component={NoteScreen} />
-					<RootNav.Screen name="Routines" component={RoutineScreen} />
-					<RootNav.Screen name="Calculator" component={CalculatorScreen} />
-					<RootNav.Screen name="Unit Converter" component={ConverterScreen} />
-					<RootNav.Screen name="Settings" component={SettingsScreen} />
+				<StatusBar backgroundColor='#0e0e0e' />
+				<RootNav.Navigator drawerContent={customContent}>
+					<RootNav.Screen
+						component={TodoScreen}
+						name='todos'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={NoteScreen}
+						name='notes'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={RoutineScreen}
+						name='routines'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={CalculatorScreen}
+						name='calculator'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={ConverterScreen}
+						name='converter'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={AccountScreen}
+						name='account'
+						options={{}}
+					/>
+					<RootNav.Screen
+						component={SettingsScreen}
+						name='settings'
+						options={{}}
+					/>
 				</RootNav.Navigator>
 			</NavigationContainer>
 		);
@@ -37,7 +107,7 @@ class AppNav extends React.Component<ReduxProps> {
 }
 
 const mapStateToProps = (state: ReduxProps) => ({
-    settings: state.settings,
+	settings: state.settings,
 });
 
 export default connect(mapStateToProps)(AppNav);
