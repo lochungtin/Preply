56a3611066ef7606b9760946786b04ab01df91e5
lochungtin <lochungtin@gmail.com>
2021-06-06 18:16:04 +0100

    update redux

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
-import { darkModeColorScheme } from '../data/colors';
 
-import { ActionType, NoteType, RoutineType, SettingsType, TagType, TodoType, } from '../types';
+import { ActionType, NoteType, TagType, TodoType, } from '../types';
 import { deleteByKey, replaceByKey } from '../utils/arrayFn';
-import { ADD_NOTE, ADD_ROUTINE, ADD_TAG, ADD_TODO, DELETE_NOTE, DELETE_ROUTINE, DELETE_TAG, DELETE_TODO, EDIT_NOTE, EDIT_ROUTINE, EDIT_TAG, EDIT_TODO, RESET_SETTINGS, SET_SETTINGS } from './action';
+import { ADD_NOTE, ADD_TAG, ADD_TODO, DELETE_NOTE, DELETE_TAG, DELETE_TODO, EDIT_NOTE, EDIT_TAG, EDIT_TODO, } from './action';
 
 const defaultNoteState: Array<NoteType> = [];
 const updateNotes = (noteState = defaultNoteState, action: ActionType) => {
@@ -20,24 +19,6 @@ const updateNotes = (noteState = defaultNoteState, action: ActionType) => {
             return update;
         default:
             return noteState;
-    }
-}
-
-const defaultRoutineState: Array<RoutineType> = [];
-const updateRoutines = (routineState = defaultRoutineState, action: ActionType) => {
-    let update = [...routineState];
-    switch (action.type) {
-        case ADD_ROUTINE:
-            update.push(action.payload);
-            return update;
-        case DELETE_ROUTINE:
-            deleteByKey(update, action.payload);
-            return update;
-        case EDIT_ROUTINE:
-            replaceByKey(update, action.payload);
-            return update;
-        default:
-            return routineState;
     }
 }
 
@@ -77,25 +58,8 @@ const updateTodos = (todoState = defaultTodoState, action: ActionType) => {
     }
 }
 
-const defaultSettingsState: SettingsType = {
-    colorScheme: darkModeColorScheme,
-    darkMode: true,
-}
-const updateSettings = (settingsState = defaultSettingsState, action: ActionType) => {
-    switch (action.type) {
-        case SET_SETTINGS:
-            return action.payload;
-        case RESET_SETTINGS:
-            return defaultSettingsState;
-        default:
-            return settingsState;
-    }
-}
-
 export default combineReducers({
     notes: updateNotes,
-    routines: updateRoutines,
-    settings: updateSettings,
     tags: updateTags,
     todos: updateTodos,
 });
