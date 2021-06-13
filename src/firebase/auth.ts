664b9606897e7a0ec234820680e2683aeb3eb476
lochungtin <lochungtin@gmail.com>
2021-06-13 20:30:50 +0100

    firebase separate keylist storage

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,5 +1,3 @@
-import firebase from 'firebase';
-
 import firebaseConfig from './config';
 
 export const resetPswd = async (email: string) => 
--- src/firebase/data.ts
+++ src/firebase/data.ts
@@ -16,11 +16,43 @@ const defErrCallback = (err: Error | null) => {
         });
 }
 
-export const firebaseDeleteTodo = (uid: string, todoKey: string, callback: ((err: Error | null) => any) = defErrCallback) =>
-    db.ref(`/UserData/${uid}/todos/${todoKey}`).remove(callback);
+// add actions
+export const firebaseAddNote = (uid: string, payload: NoteType, callback: ((err: Error | null) => any) = defErrCallback) => {
+    let update: any = {};
 
-export const firebaseDeleteNote = (uid: string, noteKey: string, callback: ((err: Error | null) => any) = defErrCallback) =>
-    db.ref(`/UserData/${uid}/notes/${noteKey}`).remove(callback);
+    update[`/UserData/${uid}/notekeys/${payload.key}`] = true;
+    update[`/UserData/${uid}/notes/${payload.key}`] = payload;
+
+    db.ref().update(update, callback);
+}
+
+export const firebaseAddTodo = (uid: string, payload: TodoType, callback: ((err: Error | null) => any) = defErrCallback) => {
+    let update: any = {};
+
+    update[`/UserData/${uid}/todokeys/${payload.key}`] = true;
+    update[`/UserData/${uid}/todos/${payload.key}`] = payload;
+
+    db.ref().update(update, callback);
+}
+
+// delete actions
+export const firebaseDeleteNote = (uid: string, noteKey: string, callback: ((err: Error | null) => any) = defErrCallback) => {
+    let update: any = {};
+
+    update[`/UserData/${uid}/notekeys/${noteKey}`] = null;
+    update[`/UserData/${uid}/notes/${noteKey}`] = null;
+
+    db.ref().update(update, callback);
+}
+
+export const firebaseDeleteTodo = (uid: string, todoKey: string, callback: ((err: Error | null) => any) = defErrCallback) => {
+    let update: any = {};
+
+    update[`/UserData/${uid}/todokeys/${todoKey}`] = null;
+    update[`/UserData/${uid}/todos/${todoKey}`] = null;
+
+    db.ref().update(update, callback);
+}    
 
 export const firebaseOverwriteUserData = (uid: string, payload: { notes: NoteMap, todos: TodoMap }, callback: ((err: Error | null) => any) = defErrCallback) => {
     let update: any = {};
@@ -31,8 +63,8 @@ export const firebaseOverwriteUserData = (uid: string, payload: { notes: NoteMap
     db.ref().update(update, callback);
 }
 
-export const firebaseSetTodo = (uid: string, payload: TodoType, callback: ((err: Error | null) => any) = defErrCallback) =>
-    db.ref(`/UserData/${uid}/todos/${payload.key}`).set(payload, callback);
-
 export const firebaseSetNote = (uid: string, payload: NoteType, callback: ((err: Error | null) => any) = defErrCallback) =>
     db.ref(`/UserData/${uid}/notes/${payload.key}`).set(payload, callback);
+
+export const firebaseSetTodo = (uid: string, payload: TodoType, callback: ((err: Error | null) => any) = defErrCallback) =>
+    db.ref(`/UserData/${uid}/todos/${payload.key}`).set(payload, callback);
